48d7ec51da8fb315aefdc57ef7096cd5d1c4255c
Viachaslau <slava.golovchik1903@gmail.com>
2023-03-17 07:44:56 +0100

    Blog rev.3

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic.edit import CreateView
+from django.views.generic.base import TemplateView
 
 from .models import Post
+from .forms import PostForm
 
 
 # Create your views here.
 
 all_posts = Post.objects.all()
+
+class PostAdd(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = "blog/add_post.html"
+    success_url = "posts/"
+
+class Report(TemplateView):
+    template_name = "blod/posts.html"
+
+    def get_context_data(self, **kwargs):
+        context =  super().get_context_data(**kwargs)
+        # context['message'] = "This works"
+        return context
+
 
 def starting_page(request):
     latest_posts = all_posts.order_by("-data")[:3]
