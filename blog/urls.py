e568eac817389d3a41452f21ab43a4c75ce10ac4
Viachaslau <slava.golovchik1903@gmail.com>
2023-03-18 21:59:13 +0100

    Blog rev.4

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.starting_page, name='starting-page'),
-    path("posts/", views.posts, name='posts-page'),
-    path("posts/add-post", views.PostAdd.as_view()),
+    path("posts", views.posts, name='posts-page'),
+    path("posts/add-post", views.PostAdd.as_view(), name='add-post'),
     path("posts/<slug:slug>", views.post_detail, 
         name='post-detail-page') #/posts/my-first-post
 ]
